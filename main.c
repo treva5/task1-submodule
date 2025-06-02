9ae23797fc30391171814a8df0c5847a1b316679
treva5 <trevamukisa5@gmail.com>
2025-06-02 13:08:10 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,8 +1,18 @@
 #include<stdio.h>
-#include<stlib.h>
+
 
 int main()
 {
+int age = 20;
+float height = 5.9;
+double distance = 12345.679;
+char initial = 'A';
+
+
+printf("The age is %d\n\n",age);
+printf("The height is %f\n\n",height);
+printf("The distance is %.2lf\n\n",distance);
+printf("The initial is %c",initial);
 
     return 0;
 }
