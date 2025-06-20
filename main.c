141cb6ee784a6883820946b64672b3bc45b7006c
treva5 <trevamukisa5@gamil.com>
2025-06-20 14:37:45 +0300

    first commit

---
+++ main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+int main ()
+{
+  int age = 20;
+  float height = 5.9;
+  double distance = 12345.6789;
+  char initial='A';
+
+  printf("The s%d",age);
+  printf("The height is %f",height);
+  printf("T
+          %lf",distance)
+   return 0;
+}
+
+
+
+
+/*Declare an integer variable named age and assign it the value 20.
+2. Declare a float variable named height and assign it the value 5.9.
+3. Declare a double variable named distance and assign it the value 12345.6789.
+4. Declare a character variable named initial and assign it the value &#39;A&#39;.
+
+Your task is to write the following code:
+- Declare the variables as mentioned.
+- Assign the specified values to the variables.
+- Print out the values of each variable using printf.*/
